94d3c6876cbce5d9962cb14d8ad16c8695c325ba
Hugo Molander <hugo.molander@gmail.com>
2022-02-03 13:29:01 +0100

    completed 2.2 Parameters

--- P1MethodsHM/Program.cs
+++ P1MethodsHM/Program.cs
@@ -1,27 +1,17 @@
-﻿static void CountUp(int to)
+﻿static void Count(int from, int to)
 {
-     for (int i = 0; i <= to; i++)
-     {
-          Console.WriteLine(i);
-     }
- 
+    Console.WriteLine("counting from " + from + " to " + to + ": ");
+    if (from < to)
+    {
+        for (int i = from; i <= to; i++) Console.WriteLine(i);
+    }
+    else
+    {
+        for (int i = from; i >= to; i--) Console.WriteLine(i);
+    } 
 }
-static void CountDown(int from)
-{
-     for (int i = from; i > -1; i--)
-     {
-          Console.WriteLine(i);
-     }
-}
-Console.WriteLine("counting from 0 to 1");
-CountUp(1);
-Console.WriteLine("counting from 1 to 0");
-CountDown(1);
-Console.WriteLine("counting from 0 to 2");
-CountUp(2);
-Console.WriteLine("counting from 2 to 0");
-CountDown(2);
-Console.WriteLine("counting from 0 to 3");
-CountUp(3);
-Console.WriteLine("counting from 3 to 0");
-CountDown(3);
+Count(2,3);
+Count(100,101);
+Count(10,8);
+Count(1,-1);
+Count(1337,1337);
