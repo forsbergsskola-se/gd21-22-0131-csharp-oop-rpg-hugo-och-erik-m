abad10dfb4663f8c38f9e9acb81ea718379958dc
ErikBigMan <erik.mattsson@student.forsbergsskola.se>
2022-02-07 15:58:27 +0100

    P1ObjectOrientedProgramming
    complet

---
+++ P1ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] Names = new String [3];
+
+ for(int i = 0; i < Names.Length; i++)
+ {
+  Console.WriteLine("Give a name.");
+  Names[i] = Console.ReadLine();
+ }
+
+Console.WriteLine(Names[0]+","+Names[1]+","+Names[2]);
