1b46dc98b90b1ee7c34c656afdfb0c8cd5c1b994
Hugo Molander <hugo.molander@gmail.com>
2022-02-14 19:16:32 +0100

    Finished with work today
    Put 1 hour of coding into project

--- projects/RPG/Program.cs
+++ projects/RPG/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static class Program  //Main program
+{
+    static void Main()
+    {
+        Unit Zombie = new Unit("Zombie",200);
+        Unit Skeleton = new Unit("Skeleton",300);
+        Unit Necromancer = new Unit("Necromancer",400);
+
+    }
+}
+
+public class Unit  //Class for units, whithin body is the constructor
+{
+    public string name;
+    public int id;
+    public static int nextId;
+    private int maxHealth;
+    private int health;
+    
+    public Unit(string _name, int maxHealth)  //Constructor start
+    {
+        name = _name;
+        id = nextId;
+        nextId++;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        ReportStatus(); //This is where constructor ends!
+    }
+
+    public void SetHealth(int newHealth)
+    {
+
+        health = Math.Clamp(newHealth, 0, maxHealth);
+        ReportStatus();
+
+    }
+    
+    
+    
+    public void ReportStatus()
+    {
+        Console.WriteLine($"Unit #{id}: {name} - {health}/{maxHealth} Health");
+    }
+        
+    
+}
