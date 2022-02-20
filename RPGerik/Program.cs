ca78d6669c76c1dfc96ea30ad087a2bf69c28b5f
ErikBigMan <erik.mattsson@student.forsbergsskola.se>
2022-02-20 16:35:06 +0100

    7 - Constructor

---
+++ RPGerik/Program.cs
@@ -0,0 +1,31 @@
+﻿static class Program
+{
+    static void Main()
+    {
+        Unit goblin = new Unit("Goblin");
+        Unit hobgoblin = new Unit("HobGoblin");
+        Unit goblinpriest = new Unit("GoblinPriest");
+    }
+}
+
+public class Unit
+{
+    public string name;
+    public int Id;
+    public static int nextId;
+
+
+    public Unit(string name)
+    {
+        this.name = name;
+        Id = nextId++;
+        
+        // Make sure, that this is the last line of the constructor:
+        ReportStatus();
+    }
+
+    public void ReportStatus()
+    {  
+        Console.WriteLine($"Unit #{Id}: {name}");
+    }
+}
